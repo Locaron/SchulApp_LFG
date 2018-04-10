6a1c3c3c3f5685ca1d77f78cbac1c55e44ee877c
Locaron <shaeq.haidari@gmail.com>
2018-04-10 18:56:38 +0200

    Starterpaket AndroidStudio

--- app/src/main/java/lfg/locaron/schulapp_lfg/MainActivity.java
+++ app/src/main/java/lfg/locaron/schulapp_lfg/MainActivity.java
@@ -1,7 +1,9 @@
 package lfg.locaron.schulapp_lfg;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+   private void toNotenrechner(View view){
+       Intent intent = new Intent(this, Notenrechner.class);
+       startActivity(intent);
+   }
 }
---
+++ app/src/main/java/lfg/locaron/schulapp_lfg/Notenrechner.java
@@ -0,0 +1,21 @@
+package lfg.locaron.schulapp_lfg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Notenrechner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notenrechner);
+    }
+
+    private int[] matheMündl = new int[20];
+    private int[] matheSchrif = new int[20];
+
+    int addNoteMathe(View view){
+    return 0;
+    }
+}
