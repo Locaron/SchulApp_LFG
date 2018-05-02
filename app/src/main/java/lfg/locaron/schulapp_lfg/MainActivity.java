ae39f7c13c719e8a157e7a04a570290a0813e299
alexk <alex.kroetz@gmail.com>
2018-05-02 15:51:59 +0200

    Bugs mit der MainActivity sin noch zu fixen ihr Menschen

--- app/src/main/java/lfg/locaron/schulapp_lfg/MainActivity.java
+++ app/src/main/java/lfg/locaron/schulapp_lfg/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
    private void toNotenrechner(View view){
        Intent intent = new Intent(this, Notenrechner.class);
        startActivity(intent);
--- app/src/main/java/lfg/locaron/schulapp_lfg/Notenrechner.java
+++ app/src/main/java/lfg/locaron/schulapp_lfg/Notenrechner.java
@@ -13,22 +13,25 @@ public class Notenrechner extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notenrechner);
+       setContentView(R.layout.activity_notenrechner);
 
     }
     private ArrayList<Integer>Mathe = new ArrayList<>();
     private int[] matheMündl = new int[20];
     private int[] matheSchrif = new int[20];
 
-    private void addNote(View v){
+    private void addNote(View view){
+        /*
         EditText input = new EditText(this);
         input.setInputType(InputType.TYPE_CLASS_NUMBER);
         //arraylists?/2 cursor?
-        switch(v.getId()){
+        switch(view.getId()){
             case R.id.MatheXD:
                 String test = input.getText().toString();
                 Mathe.add(Integer.parseInt(test));
                 break;
         }
+        */
     }
+
 }
