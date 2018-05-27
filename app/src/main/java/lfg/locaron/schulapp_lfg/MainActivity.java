abfa05e6199feb0cce0c65f5dfc5337094581912
Locaron <shaeq.haidari@gmail.com>
2018-05-27 15:51:12 +0200

    Starterpaket AndroidStudio

--- app/src/main/java/lfg/locaron/schulapp_lfg/MainActivity.java
+++ app/src/main/java/lfg/locaron/schulapp_lfg/MainActivity.java
@@ -4,17 +4,27 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonToNotenrechner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-   private void toNotenrechner(View view){
-       Intent intent = new Intent(this, Notenrechner.class);
-       startActivity(intent);
-   }
+        buttonToNotenrechner = (Button) findViewById(R.id.buttonToNotenrechner);
+        buttonToNotenrechner.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toNotenrechnerMain(view);
+            }
+        });
+    }
+    private void toNotenrechnerMain(View view){
+        Intent intent = new Intent(this, NotenrechnerMain.class);
+        startActivity(intent);
+    }
 }
