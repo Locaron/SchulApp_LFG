4e70b94c9b77df85eb7efcebbf6bb4ddf49e6940
ZwergXXL <zwergxxl@gmx.at>
2018-06-02 19:31:20 +0200

    add SQL

--- app/src/main/java/lfg/locaron/schulapp_lfg/Activity/MainActivity.java
+++ app/src/main/java/lfg/locaron/schulapp_lfg/Activity/MainActivity.java
@@ -1,4 +1,4 @@
-package lfg.locaron.schulapp_lfg;
+package lfg.locaron.schulapp_lfg.Activity;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -6,9 +6,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lfg.locaron.schulapp_lfg.R;
+import lfg.locaron.schulapp_lfg.SQLite.SQL;
+import lfg.locaron.schulapp_lfg.SQLite.SQLData;
+
 public class MainActivity extends AppCompatActivity {
 
     Button buttonToNotenrechner;
+    SQL sql;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +30,28 @@ public class MainActivity extends AppCompatActivity {
                 toNotenrechnerMain(view);
             }
         });
+
+        /*
+        sql = new SQL(this);
+        sql.open();
+
+        List<Integer> list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(3);
+
+        List<Integer> list2 = new ArrayList<>();
+        list2.add(3);
+        list2.add(5);
+        sql.write(new SQLData(list1, list2, 0));
+
+        SQLData data = sql.read(0);
+        Log.d("ok", data.listToString(data.getMuendlich()));
+        sql.close();
+
+        */
+
     }
+
     private void toNotenrechnerMain(View view){
         Intent intent = new Intent(this, NotenrechnerMain.class);
         startActivity(intent);
---
+++ app/src/main/java/lfg/locaron/schulapp_lfg/SQLite/Fach.java
@@ -0,0 +1,20 @@
+package lfg.locaron.schulapp_lfg.SQLite;
+
+public interface Fach {
+  int FRANZOESISCH = 0;
+  int ENGLISCH = 1;
+  int DEUTSCH  = 2;
+  int MATHE  = 3;
+  int PHYSIK = 4;
+  int CHEMIE = 5;
+  int BIOLOGIE = 6;
+  int INFORMATIK = 7;
+  int NuT = 8;
+  int WR = 9;
+  int ERDKUNDE = 10;
+  int SPORT = 11;
+  int KUNST = 12;
+  int MUSIK = 13;
+  int LATEIN = 14;
+  int RELIGION = 15;
+}
